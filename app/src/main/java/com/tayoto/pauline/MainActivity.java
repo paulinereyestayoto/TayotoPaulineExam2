1d273d7730b05d73366147a8c7fca11f325553c3
Pauline Tayoto <paulinetayoto@gmail.com>
2018-10-04 16:00:53 +0800

    .

--- app/src/main/java/com/tayoto/pauline/MainActivity.java
+++ app/src/main/java/com/tayoto/pauline/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, MyCustomService.class);
+        startService(i);
     }
 
     public void process(View v){
